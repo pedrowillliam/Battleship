10984a73117a3f39b3e17dfe7986d95f182125bf
ellenmariadev <ellenmariadev@gmail.com>
2025-03-18 18:47:17 -0300

    feat(page): game page styling

---
+++ frontend/api/login.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".login-form");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = form.querySelector('input[type="text"]').value;
+    const password = form.querySelector('input[type="password"]').value;
+
+    if (!username || !password) {
+      alert("Preencha todos os campos!");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert(data.message);
+        console.log("Registro realizado com sucesso", data);
+        localStorage.setItem("username", username);
+        window.location.href = "/frontend/battleship/game.html";
+      } else {
+        alert(data.message || "Erro no login!");
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+      alert("Erro na conexão com o servidor.");
+    }
+  });
+});
--- frontend/script/board.js
+++ frontend/script/board.js
@@ -1,7 +1,33 @@
 //Lógica do tabuleiro (posicionamento aleatorio dos navios, grid 10x10, etc)
 
-document.querySelectorAll(".cell").forEach((cell) => {
-  cell.addEventListener("click", () => {
-    cell.classList.toggle("miss");
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("../components/board.html", "board-component");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("../components/board.html", "board-component-two");
+});
+
+function loadComponent(url, elementId, callback) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+      if (callback) callback();
+    })
+    .catch((error) => console.error(error));
+}
+
+document.getElementById("surrender-btn").addEventListener("click", () => {
+  localStorage.removeItem("username");
+  window.location.href = "/frontend/battleship/login/login.html";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const username = localStorage.getItem("username");
+  if (!username) {
+    window.location.href = "/frontend/battleship/login/login.html";
+    return;
+  } 
+  document.querySelector(".half.blue .username").textContent = '@' + username;
 });
