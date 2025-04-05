7146221d7180a523ce2f5cfb82745e1477fec43a
pedrowillliam <pedrowilliam014@hotmail.com>
2025-04-05 20:22:31 -0300

    ultima review dessa task Deus quiser.

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,6 +1,6 @@
     function iniciarJogo() {
     //sair da paginia inicial para a de login por esse botão
     //se nao funcionar por esse caminho avisa
-    window.location.href = "/Battleship/frontend/battleship/login/login.html"; 
+    window.location.href = "./frontend/battleship/login/login.html"; 
   }
   
